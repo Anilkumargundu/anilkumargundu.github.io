ae8cb361bba1c8c7548b71520f92bf79591f0260
Anil Kumar Gundu <gunduanilk@gmail.com>
2025-06-22 21:49:42 +0800

    Update 2nd_order_system_streamlit.py

--- _resources/2nd_order_system_streamlit.py
+++ _resources/2nd_order_system_streamlit.py
@@ -113,7 +113,7 @@ except Exception as e:
 st.markdown(
     """
     <hr style="margin-top: 3em; margin-bottom: 0.5em">
-    <div style='text-align: center; font-size: 0.9em; color: gray;'>
+    <div style='text-align: center; font-size: 9em; color: black;'>
         © 2025 Anil Kumar Gundu | 
         <a href="https://anilkumargundu.github.io" target="_blank">Visit My Website</a>
     </div>
